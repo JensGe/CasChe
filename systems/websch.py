71d1bd694bba071626d9da0619ffb3b0eff76a1d
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-06-23 21:57:41 +0200

    use fqdn_hash_range for exampleDB waiting

--- systems/websch.py
+++ systems/websch.py
@@ -63,12 +63,12 @@ def wait_for_example_db(settings):
         )
         url_reached = example_db["url_amount"] >= url_th
 
-        hash_reached = example_db
+        hash_reached = example_db["fqdn_hash_range"] > 0
 
-        if fqdn_reached and url_reached:
+        if fqdn_reached and url_reached and hash_reached:
             print(
-                "** Threshold {} reached: url_amount={}".format(
-                    url_th, example_db["url_amount"]
+                "** Threshold {} reached: url_amount={}, hash_range={}".format(
+                    url_th, example_db["url_amount"], example_db["fqdn_hash_range"]
                 )
             )
             done = True
